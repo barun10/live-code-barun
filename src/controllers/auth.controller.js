fe2b28c136526c7a6be021b7c8dead34938a3c67
barun10 <36666345+barun10@users.noreply.github.com>
2023-08-10 05:57:33 +0000

    user logged in and out

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -46,3 +46,48 @@ export const signUp = asyncHandler(async(req, res) => {
     })
 
 })
+
+export const logIn = asyncHandler(async(req, res) => {
+        //get data from user
+
+        const { email, password } = req.body
+
+        //validation
+    
+        if (!email || !password){
+            throw new CustomError("Please fill all details", 400)
+        }
+        
+        const user = await User.findOne({email}).select("+password")
+
+        if(!user){
+            throw new CustomError("Invalid credentials", 400)
+        }
+
+        const isPasswordMatched = await user.comparePassword(password)
+
+        if(isPasswordMatched){
+            const token = user.getJWTtoken()
+            user.password = undefined
+            res.cookie("token", token, cookieOptions)
+            return res.status(200).json({
+                success: true,
+                token,
+                user
+            })
+        }
+
+        throw new CustomError("Password is incorrect", 400)
+})
+
+export const logOut = asyncHandler(async(req, res) => {
+    res.cookie("token", null, {
+        expires: new Date(Date.now()),
+        httpOnly: true
+    })
+
+    res.status(200).json({
+        success: true,
+        message: "Logged Out"
+    })
+})
