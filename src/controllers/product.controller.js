6e99af7deb77ac72a69f1f1500045427a719e347
barun10 <36666345+barun10@users.noreply.github.com>
2023-09-05 04:22:05 +0000

    photo upload and delete from aws

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -23,7 +23,7 @@ export const addProduct = asyncHandler(async (req, res) => {
       throw new CustomError("Please fill all the fields", 500)
       }
 
-    let imgArrayResp = Promise.all(
+    let imgArrayResponse = Promise.all(
       Object.keys(files).map( async (file, index) => {
         const element = file[fileKey]
         console.log(element);
@@ -62,3 +62,72 @@ export const addProduct = asyncHandler(async (req, res) => {
   })
 })
 
+export const getAllProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({})
+
+  if (!products) {
+    throw new CustomError("No product is found", 404)
+  }
+
+  res.status(200).json({
+    success: true,
+    products
+  })
+})
+
+export const getProductById = asyncHandler(async (req, res) => {
+
+  const {id: productId} = req.params
+  const product = await Product.findById(productId)
+
+  if (!product) {
+    throw new CustomError("No product is found", 404)
+  }
+
+  res.status(200).json({
+    success: true,
+    product
+  })
+})
+
+export const getProductByCollectionId = asyncHandler(async (req, res) => {
+  const {id: collectionId} = req.params
+
+  const products = await Product.find({collectionId})
+
+  if (!products) {
+    throw new CustomError("No product is found", 404)
+  }
+  res.status(200).json({
+    success: true,
+    products
+  })
+})
+
+export const deleteProduct = asyncHandler(async(req, res) => {
+  const {id: productId} = req.params
+
+  const product = await Product.findById(productId)
+
+  if (!product) {
+    throw new CustomError("No product is found", 404)
+  }
+
+  const deletePhotos = Promise.all(
+    product.photos.map(async(elem, index) => {
+      await s3deleteFile({
+        bucketName: config.S3_BUCKET_NAME,
+        key: `products/${product._id.toString()}/photo_${index + 1}.png`
+      })
+    })
+  )
+
+  await deletePhotos
+
+  await product.remove()
+
+  res.status(200).json({
+    success: true, 
+    message: "Product has been deleted successfully"
+  })
+})
