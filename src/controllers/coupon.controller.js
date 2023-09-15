cbe359becf2d8760b4f18e263bc6fa063067e77e
barun10 <36666345+barun10@users.noreply.github.com>
2023-09-15 04:52:27 +0000

    coupon controller done

--- src/controllers/coupon.controller.js
+++ src/controllers/coupon.controller.js
@@ -34,7 +34,44 @@ export const getAllCoupon = asyncHandler(async(req, res) => {
     })
 })
 
-// export const updateCoupon = asyncHandler(async(req, res) => {
-//     const {code, discount} = req.body
-//     const {id} = req.params
-// })
+export const updateCoupon = asyncHandler(async(req, res) => {
+    const {action} = req.body
+    const {id: couponId} = req.params
+
+    if(!code || !discount){
+        throw new CustomError("code or discount requires", 400)
+    }
+
+    const coupon = Coupon.findByIdAndUpdate(couponId, {
+        active: action
+    },{
+        new: true,
+        runValidators: true
+    })
+
+    if (!updateCoupon) {
+        throw new CustomError("Coupon not found", 404)
+    }
+
+    res.status(200).json({
+        success: true,
+        message: "Coupon was updated successfully",
+        coupon
+    })
+})
+
+export const deleteCoupon = asyncHandler(async(req, res)=> {
+    const {id: couponId} = req.params
+
+    const coupon = await Coupon.findByIdAndDelete(couponId)
+
+    if(!coupon){
+        throw new CustomError("Coupon is not found", 404)
+    }
+
+    res.status(200).json({
+        success: true,
+        message: "coupon deleted successfully"
+    })
+    
+})
